652cd986cd683d4914ac2b27ffaf03740e36e0a7
Matizin46 <mattheus.paes.br@gmail.com>
2024-11-21 21:30:03 -0300

    Atividade em C# para calcular  notas de alunos

---
+++ Notas_do_Aluno/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Notas_do_Aluno
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // Solicitar o número de faltas ou 'S' para sair
+            Console.Write("Digite o número de faltas ou 'S' para sair: ");
+            string inputFalta = Console.ReadLine();
+
+            if (inputFalta.ToUpper() == "S")
+            {
+                Console.WriteLine("Programa encerrado.");
+            }
+
+            // Solicita a média para o úsuario
+            Console.Write($"Digite sua média ou 'S' para sair: ");
+
+           
+
+            Console.ReadKey();
+        }
+    }
+}
